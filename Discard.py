d19a265a503c2e294a05ba74f4286fb24c691c2f
Supertos <57270422+Supertos@users.noreply.github.com>
2022-03-28 14:46:47 +0300

    Create socket on start

--- Discard.py
+++ Discard.py
@@ -10,11 +10,13 @@
 import network_base
 import random
 import os
+import socket
 GLOBAL_ENCODER = network_base.msg_encoder()
 GLOBAL_NETWORK = network_base.network_interface()
 
 GLOBAL_ENCODER.bind_interface( GLOBAL_NETWORK )
 
+GLOBAL_HOST = False
 GLOBAL_VER = "0.1"
 GLOBAL_SLOGANS = [
     "Now with letters!",
@@ -33,14 +35,22 @@ def cmd_help():
     print("help - show this text")
     print("connect - start connection")
     print("quit - stop discard'ing")
-    print("")
+    print("host - start server")
     print("")
     print("")
 
+def discard_tick():
+    if GLOBAL_HOST:
+        pass
+
 cmd_help()
+GLOBAL_NETWORK.createConnection( socket.gethostbyname(socket.gethostname()), random.randint(1000, 9999), "Nya" )
+print("Socket created at ", GLOBAL_NETWORK.Address, ":", GLOBAL_NETWORK.Port)
 while True:
     cmd = input("<Discard>: " )
     if cmd == "help":
         cmd_help()
     elif cmd == "quit":
         quit(0)
+    elif cmd == "host":
+        pass
