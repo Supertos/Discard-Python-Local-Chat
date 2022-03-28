a7be6551f4e61074862fb473d10501ca3711555f
Supertos <57270422+Supertos@users.noreply.github.com>
2022-03-28 14:43:13 +0300

    Quit and Help commands

--- Discard.py
+++ Discard.py
@@ -9,7 +9,7 @@
 
 import network_base
 import random
-
+import os
 GLOBAL_ENCODER = network_base.msg_encoder()
 GLOBAL_NETWORK = network_base.network_interface()
 
@@ -22,9 +22,25 @@ GLOBAL_SLOGANS = [
     "Made in Russia",
     "Nya",
     "Please delete this",
-    "Vanya, we need to implement connection stuff"
+    "Vanya, we need to implement connection"
 ]
 
 print("===DISCARD===")
 print("        ", GLOBAL_SLOGANS[ random.randint(0, len(GLOBAL_SLOGANS)-1 ) ] )
 print("VER: ", GLOBAL_VER )
+
+def cmd_help():
+    print("help - show this text")
+    print("connect - start connection")
+    print("quit - stop discard'ing")
+    print("")
+    print("")
+    print("")
+
+cmd_help()
+while True:
+    cmd = input("<Discard>: " )
+    if cmd == "help":
+        cmd_help()
+    elif cmd == "quit":
+        quit(0)
