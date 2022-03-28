91931d8bcb9c224b86af22f8fd8ce401b548bafb
Supertos <57270422+Supertos@users.noreply.github.com>
2022-03-28 14:38:43 +0300

    Added initial sequence

--- Discard.py
+++ Discard.py
@@ -8,8 +8,23 @@
 -------------------------------------------------"""
 
 import network_base
+import random
 
 GLOBAL_ENCODER = network_base.msg_encoder()
 GLOBAL_NETWORK = network_base.network_interface()
 
 GLOBAL_ENCODER.bind_interface( GLOBAL_NETWORK )
+
+GLOBAL_VER = "0.1"
+GLOBAL_SLOGANS = [
+    "Now with letters!",
+    "Free except electriciy bills",
+    "Made in Russia",
+    "Nya",
+    "Please delete this",
+    "Vanya, we need to implement connection stuff"
+]
+
+print("===DISCARD===")
+print("        ", GLOBAL_SLOGANS[ random.randint(0, len(GLOBAL_SLOGANS)-1 ) ] )
+print("VER: ", GLOBAL_VER )
